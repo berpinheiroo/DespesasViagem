868228e80d2714e01f0ddf97397f50ffec31a678
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-03-01 20:15:48 -0300

    Testes aplicados

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Parte 1
 
-function calcularGastoCombustivel(distanciaMetros, tipoCombustivel) {
+exports.calcularGastoCombustivel = function(distanciaMetros, tipoCombustivel) {
     if (!distanciaMetros || typeof distanciaMetros !== 'number' || distanciaMetros <= 0) {
         return 'Distância inválida'
     }
@@ -18,17 +18,12 @@ function calcularGastoCombustivel(distanciaMetros, tipoCombustivel) {
     }
     const distanciaKm = distanciaMetros / 1000
     const litrosNecessarios = distanciaKm / kmPorLitro
-    return Math.ceil(litrosNecessarios)
+    return (parseFloat(litrosNecessarios.toFixed(2)))
 }
-
-const distanciaMetros = 14500
-const tipoCombustivel = 'etanol'
-const litrosNecessarios = calcularGastoCombustivel(distanciaMetros, tipoCombustivel)
-console.log(litrosNecessarios)
 
 // Parte 2
 
-function calcularNumeroParadas(passageiros, duracaoHoras) {
+exports.calcularNumeroParadas = function (passageiros, duracaoHoras) {
     if (!passageiros || !Array.isArray(passageiros) || passageiros.length <= 0) {
         return 'Lista de passageiros inválida'
     }
@@ -65,71 +60,44 @@ function calcularNumeroParadas(passageiros, duracaoHoras) {
     return qtdParadas
 }
 
-const passageiros = ['adulto', 'adulto', 'crianca', 'crianca', 'adulto']
-const duracaoHoras = 5
-
-const qtdParadas = calcularNumeroParadas(passageiros, duracaoHoras)
-
-console.log(`Serão necessárias ${ qtdParadas } paradas para a viagem.`)
-
 // Parte 3
 
-function calcularValorRefeicoes(qtdParadas, passageiros) {
-    if (!Number.isInteger(qtdParadas) || qtdParadas <= 0) {
-      return 'A quantidade de paradas deve ser um número inteiro positivo.'
+exports.calcularValorRefeicoes = function(qtdParadas, passageiros) {
+    let adultos = 0
+    let criancas = 0
+    let completas = 0
+    let leves = 0
+    let completa = true
+    let gastoCriancas
+    let gastoAdultos
+
+    for (let i = 0; i < passageiros.length; i++) {  //Calcular número de adultos
+        if (passageiros[i] === "adulto") {
+            adultos++
+        }
     }
-    if (!Array.isArray(passageiros) || passageiros.length === 0) {
-      return 'A lista de passageiros deve ser um array não vazio.'
+
+    criancas = passageiros.length - adultos  //Calcular número de criancas
+
+    for (let i = 0; i < qtdParadas; i++) {  //Calcular número de refeições completas e leves
+        if (completa === true) {
+            completas++
+            completa = false
+        }
+
+        if (leves === completas + 2) {
+            completa = true
+        }
+        if (completa === false) {
+            leves++
+        }
     }
-  
-    const precoRefeicaoLeveAdulto = 30
-    const precoRefeicaoLeveCrianca = 20
-    const precoRefeicaoCompletaAdulto = 50
-    const precoRefeicaoCompletaCrianca = 40
-    let totalGasto = 0
-  
-    // Primeira parada
-    totalGasto += precoRefeicaoCompletaAdulto * countAdultos(passageiros)
-    totalGasto += precoRefeicaoCompletaCrianca * countCriancas(passageiros)
-  
-    // Cada duas paradas de refeições leves, uma é de refeições completas
-    for (let i = 2; i <= qtdParadas; i += 2) {
-      totalGasto += precoRefeicaoCompletaAdulto * countAdultos(passageiros)
-      totalGasto += precoRefeicaoCompletaCrianca * countCriancas(passageiros)
-    }
-  
-    // Demais paradas são de refeições leves
-    for (let i = 1; i < qtdParadas; i += 2) {
-      totalGasto += precoRefeicaoLeveAdulto * countAdultos(passageiros)
-      totalGasto += precoRefeicaoLeveCrianca * countCriancas(passageiros)
-    }
-  
-    return totalGasto
-  }
-  
-  function countAdultos(passageiros) {
-    if (!Array.isArray(passageiros) || passageiros.length === 0) {
-      return 0
-    }
-    return passageiros.filter(passageiro => passageiro === 'adulto').length
-  }
-  
-  function countCriancas(passageiros) {
-    if (!Array.isArray(passageiros) || passageiros.length === 0) {
-      return 0
-    }
-    return passageiros.filter(passageiro => passageiro === 'crianca').length
-  }
-  
-  // (Dados reutilizados da parte 2)
-  if (typeof qtdParadas === 'string') {
-    console.log(qtdParadas)
-  } else {
-    const gastosRefeicoes = calcularValorRefeicoes(qtdParadas, passageiros)
-    if (typeof gastosRefeicoes === 'string') {
-      console.log(gastosRefeicoes)
-    } else {
-      console.log(`O gasto total com refeições será de R$${gastosRefeicoes}.`)
-    }
-  }
+    //Calcular gasto por adultos e criancas
+    gastoAdultos = adultos * (leves * 30 + completas * 50)
+    gastoCriancas = criancas * (leves * 20 + completas * 40)
+
+    //Calcular gasto total
+    let gastoTotal = gastoAdultos + gastoCriancas
+    return (gastoTotal)
+}
   
